918f16b5955610718cc98b7c9f685acd6c42b560
Federico Peluffo <fedepel@gmail.com>
2023-04-08 17:25:59 -0300

    desafioWebsockets

--- Desafios/4-cuartoDesafio/src/public/js/realTimeProd.js
+++ Desafios/4-cuartoDesafio/src/public/js/realTimeProd.js
@@ -2,47 +2,8 @@ const socket = io();
 
 const tbodyProducts = document.getElementById(`tbodyProducts`);
 
-socket.on(`refreshTable`, data => {
-    product = `
-    {{#each allProducts}}
-    <tr>
-        <th scope="row">
-            {{this.id}}
-        </th>
-        <td>
-            {{this.title}}
-        </td>
-        <td>
-            {{this.price}}
-        </td>
-        <td>
-            {{this.stock}}
-        </td>
-    </tr>
-    {{/each }}
-    `;
-    tbodyProducts.innerHTML += product;
-});
-
 socket.on(`allProducts`, data => {
+    console.log(data);
 
-    data.forEach(product => {
-        product = `
-            <tr>
-                <th scope="row">
-                    ${product.id}
-                </th>
-                <td>
-                    ${product.title}
-                </td>
-                <td>
-                    ${product.price} 
-                </td>
-                <td>
-                    ${product.stock} 
-                </td>
-            </tr>
-        `;
-        tbodyProducts.innerHTML += product;
-    });
+
 });
